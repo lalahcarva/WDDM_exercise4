9dc588f3976157a4dd9047b7f75755db6c22d085
Larissa <lah_c_r@hotmail.com>
2023-03-30 12:10:07 -0400

    first

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+console.log("This is a test log");
 
+function changeColor() {
+    document.getElementById("heart").style.color = "red";
+}
+
+function showImg() {
+    document.getElementById("dog").style.visibility = "visible";
+}
